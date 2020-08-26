ba7938300e4ff35d1ff434c0e70bda620f68fef3
jun <enskawk1991@gmail.com>
2020-08-27 01:02:56 +0900

    add deleteMoive function

--- graphQL/resolver.js
+++ graphQL/resolver.js
@@ -7,9 +7,10 @@ const resolvers = {
   },
   Mutation: {
     addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
-
+// body parser를 쓸필요도 없고 request response를 쓸필요도없고.... 엄청 간다해지는데..? 서버에서 할게없어.
 // Resolver Query.name must be object or function 오키도키
 // localhost:4000 은 playGround 즉 GraphQL의 postman 같은거라고 생각 하면 됨.
 //GraphQL 서버가 Query 나 Mutation 같은 정의를 발견하면. resolver로 와서 정보를 찾음.
